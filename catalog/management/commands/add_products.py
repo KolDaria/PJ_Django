30c30d29d87017c29768e3a6c4d7783b45a64964
Daria Kolisnyk <dashamel17@gmail.com>
2025-05-16 22:17:13 +0300

    Создание фикстур и миграций

---
+++ catalog/management/commands/add_products.py
@@ -0,0 +1,16 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+from catalog.models import Category, Product
+
+
+class Command(BaseCommand):
+    help = 'Load test data from fixture'
+
+    def handle(self, *args, **kwargs):
+        Category.objects.all().delete()
+        Product.objects.all().delete()
+
+        call_command('loaddata', 'categorys_fixture.json')
+        call_command('loaddata', 'products_fixture.json')
+        self.stdout.write(self.style.SUCCESS('Successfully loaded data from fixture'))
