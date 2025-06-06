3f0af6caaf00f8a8831b76d3c75090f0c4a5fa1c
Daria Kolisnyk <dashamel17@gmail.com>
2025-06-06 12:47:26 +0300

    Реализована валидация, стилизация форм

---
+++ catalog/forms.py
@@ -0,0 +1,56 @@
+from django.core.exceptions import ValidationError
+from django.forms import BooleanField, ModelForm
+
+from catalog.models import Product
+
+FORBIDDEN_WORDS = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fild_name, fild in self.fields.items():
+            if isinstance(fild, BooleanField):
+                fild.widget.attrs['class'] = 'form-check-input'
+            else:
+                fild.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, ModelForm):
+    is_published = BooleanField(label="Опубликовать", required=False)
+
+    def clean_price(self):
+        purchase_price = self.cleaned_data.get('purchase_price')
+        if purchase_price < 0:
+            raise ValidationError('Цена введена неправильно')
+        return purchase_price
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        lower_forbidden = [forbidden.lower() for forbidden in FORBIDDEN_WORDS]
+        if name.lower() in lower_forbidden:
+            raise ValidationError('Неуместное наименование товара')
+        return name
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        lower_forbidden = [forbidden.lower() for forbidden in FORBIDDEN_WORDS]
+        if description.lower() in lower_forbidden:
+            raise ValidationError('Неуместное описание товара')
+        return description
+
+    def clean_image(self):
+        image = self.cleaned_data.get('image')
+        extension = image.name.split('.')[-1].lower()
+        if extension not in ['jpg', 'jpeg', 'png']:
+            raise ValidationError('Допустимы только форматы JPEG и PNG.')
+
+        max_size = 5 * 1024 * 1024  # 5MB
+        if image.size > max_size:
+            raise ValidationError('Размер изображения не должен превышать 5MB.')
+
+        return image
+
+    class Meta:
+        model = Product
+        fields = "__all__"
