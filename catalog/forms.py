5e35f34bdd75f39ccd04487f621cd93dc9a2dd19
Daria Kolisnyk <dashamel17@gmail.com>
2025-06-07 11:48:46 +0300

    Редактирование валидации в форме товара

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,9 +1,12 @@
+import re
+
 from django.core.exceptions import ValidationError
 from django.forms import BooleanField, ModelForm
 
 from catalog.models import Product
 
 FORBIDDEN_WORDS = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+FORBIDDEN_PATTERN = re.compile(r'\b(' + '|'.join(FORBIDDEN_WORDS) + r')\b', re.IGNORECASE)
 
 
 class StyleFormMixin:
@@ -19,7 +22,7 @@ class StyleFormMixin:
 class ProductForm(StyleFormMixin, ModelForm):
     is_published = BooleanField(label="Опубликовать", required=False)
 
-    def clean_price(self):
+    def clean_purchase_price(self):
         purchase_price = self.cleaned_data.get('purchase_price')
         if purchase_price < 0:
             raise ValidationError('Цена введена неправильно')
@@ -27,28 +30,26 @@ class ProductForm(StyleFormMixin, ModelForm):
 
     def clean_name(self):
         name = self.cleaned_data.get('name')
-        lower_forbidden = [forbidden.lower() for forbidden in FORBIDDEN_WORDS]
-        if name.lower() in lower_forbidden:
+        if FORBIDDEN_PATTERN.search(name):
             raise ValidationError('Неуместное наименование товара')
         return name
 
     def clean_description(self):
         description = self.cleaned_data.get('description')
-        lower_forbidden = [forbidden.lower() for forbidden in FORBIDDEN_WORDS]
-        if description.lower() in lower_forbidden:
+        if FORBIDDEN_PATTERN.search(description):
             raise ValidationError('Неуместное описание товара')
         return description
 
     def clean_image(self):
         image = self.cleaned_data.get('image')
-        extension = image.name.split('.')[-1].lower()
-        if extension not in ['jpg', 'jpeg', 'png']:
-            raise ValidationError('Допустимы только форматы JPEG и PNG.')
+        if image:
+            extension = image.name.split('.')[-1].lower()
+            if extension not in ['jpg', 'jpeg', 'png']:
+                raise ValidationError('Допустимы только форматы JPEG и PNG.')
 
-        max_size = 5 * 1024 * 1024  # 5MB
-        if image.size > max_size:
-            raise ValidationError('Размер изображения не должен превышать 5MB.')
-
+            max_size = 5 * 1024 * 1024  # 5MB
+            if image.size > max_size:
+                raise ValidationError('Размер изображения не должен превышать 5MB.')
         return image
 
     class Meta:
