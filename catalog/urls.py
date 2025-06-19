9c2b0fac1b5c5fc27c0d00865923ec52093dbf38
Daria Kolisnyk <dashamel17@gmail.com>
2025-06-19 22:11:30 +0300

    Сервисная функция для работы с продуктами

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,21 +3,23 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import (ContactListListView, ContactView, HomeViewListView, ProductCreateView, ProductDeleteView,
-                           ProductDetailDetailView, ProductListListView, ProductUpdateView, unpublish_product)
+from catalog.views import (CategoryDetailView, ContactListListView, ContactView, HomeViewListView, ProductCreateView,
+                           ProductDeleteView, ProductDetailDetailView, ProductListListView, ProductUpdateView,
+                           unpublish_product)
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('products_list/', ProductListListView.as_view(), name='products_list'),
-    path('product_detail/<int:product_id>', ProductDetailDetailView.as_view(), name='product_detail'),
+    path('product_detail/<int:pk>', ProductDetailDetailView.as_view(), name='product_detail'),
     path('new/', ProductCreateView.as_view(), name='product_create'),
     path('<int:pk>/edit/', ProductUpdateView.as_view(), name='product_edit'),
     path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
     path('', HomeViewListView.as_view(), name='home_view'),
     path('contacts/', ContactListListView.as_view(), name='contact_list'),
     path('contact_view/', ContactView.as_view(), name='contact_view'),
-    path('product/<int:product_id>/unpublish/', unpublish_product, name='unpublish_product'),
+    path('product/<int:pk>/unpublish/', unpublish_product, name='unpublish_product'),
+    path('category/<int:pk>/', CategoryDetailView.as_view(), name='category_detail'),
 ]
 
 if settings.DEBUG:
